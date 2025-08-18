908e7e7656b8f46585e47a355ed4e1c7424bae00
Faveur <faveur927@gmail.com>
2025-08-18 20:14:16 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  "projectId": "superapp-mvp-proposal",
+  "appId": "1:552824195923:web:7a77ccdff6b4940da0efa6",
+  "storageBucket": "superapp-mvp-proposal.firebasestorage.app",
+  "apiKey": "AIzaSyAXNgCPFmZ8_61Ru0iZUes7HtgM1ev18oY",
+  "authDomain": "superapp-mvp-proposal.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "552824195923"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export { app };
